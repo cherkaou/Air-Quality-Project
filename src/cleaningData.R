7df7ed7badb1244e4665fb2e99db36978dc691d0
Yousra CHERKAOUI-TANGI <yousra.cherkaoui-tangi@telecom-sudparis.eu>
2020-01-07 11:39:20 +0100

    update du script cleaning.R

--- src/cleaningData.R
+++ src/cleaningData.R
@@ -9,8 +9,9 @@ data$X.1 = NULL
 
 #On supprime les lignes contenant des valeurs manquantes 
 for(i in 1:15){
-  data = subset(data, !is.na(data[i,]))
+  data = subset(data, !is.na(data[,i]))
 }
+
 
 #On met les données sous le bon format : format de date standard + nettoyage des données
 data$Date = as.Date(data$Date,format='%d/%m/%Y' )
@@ -21,10 +22,20 @@ data$RH = as.numeric(sub(",", ".", data$RH))
 data$AH = as.numeric(sub(",", ".", data$AH))
 
 
+#On supprime cette colonne car elle ne contient que des -200 globalement 
+data$NMHC.GT.=NULL
+
+#On supprime toutes les lignes contenant des -200
+for(j in 1:14){
+  data = subset(data, data[,j]!=-200)
+}
+
+
+
 #On regroupe les données par jour en supprimant le temps et en prenant la moyenne de toutes les concentrations de gaz par jour
 daily_data = copy(data)
 daily_data$Time = NULL
-daily_data = apply(daily_data[,2:14],2,tapply, daily_data$Date,mean )
+daily_data = apply(daily_data[,2:13],2,tapply, daily_data$Date,mean )
 daily_data = as.data.frame(daily_data)
 setDT(daily_data, keep.rownames = "Date")
 setDF(daily_data)
